1ee683f119cc055a5b7a12153fa289ff2438d181
marleyrosario <70246062+marleyrosario@users.noreply.github.com>
2021-11-05 07:47:00 -0500

    Update datasci_public.py

--- datasci_public.py
+++ datasci_public.py
@@ -72,7 +72,7 @@ I would also alert that in order to do this case, you need to be somewhat famili
 In a future script, I can share terminal code that you can use in Google's PowerShell or the command prompt
 of whatever package manager you are using. 
 
-Case 2 --> 
+Case 2 --> Data Science and complex Analysis 
 For all the projects strictly using BigQuery or Sheets, often specifically for ETLs, ELTs, or 
 more complex analysis projects, go to this link: https://console.cloud.google.com/
 
@@ -96,9 +96,21 @@ Below are the steps to help guide this process:
     make sure there are no weird headers. Things that could make BQ return an error would be columns with non-alphanumeric characters. 
     Step 11: Give the table a name, again no non-alphanumeric characters
     Step 12: Generally, I go to Advanced Options and set the number of errors equal to number of rows of the file I am trying to upload. 
-    This will make sure BigQuery will allow on the table in order to be uploaded in the first place. If there is a type error (an error that basically says
+    This will make sure BigQuery will allow the table to be saved in order to be uploaded in the first place. If there is a type error (an error that basically says
     the data type for a cell is incorrect relative to the rest of the column), Big Query will return it to us on a general query. 
-    
+    Step 13: Now, you should see the table appear on the left hand side directly underneath the dataset. 
+    Step 14: Once you have done this, I generally would do just a general query which is formatted like "Select * From 'project_id.dataset_id.table_name'" just to see
+    if my data will query with no problem. If you click on the large "QUERY" button (it depends on whether you are using legacy or the newer UI of the SQL Workspace for where
+    this button can be found), a general query will come up. 
+    Step 15: Now that you have set up your table and everything in Big Query you need to create an API JSON token that you can use 
+    to programmatically connect to the database. 
+    Step 16: Go back to the Google Cloud Platform side bar on the left hand side, and under Products click
+    IAM Admin and then click "Service Accounts"
+    Step 17: Click Create Service Accounts
+    Step 18: Give your service account a name. Note: You will have to do two of these - 1 for BQ and 1 for sheets. 
+    Step 19: Grant approval to at the very minimum data editor and reader, for the purposes of this project. Giving yourself
+    admin priviledges doesn't hurt either. 
+    Step 20:
 
 
 """
